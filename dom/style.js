84a5e787508bac746cfb2b15d05b6f3eb14eaa88
Marcus Westin <marcus.westin@gmail.com>
2011-08-08 11:54:40 -0700

    allow for opacity and graient to be passed in as a style property to NODEs

--- dom/style.js
+++ dom/style.js
@@ -1,44 +1,48 @@
 var client = require('std/client'),
   each = require('std/each')
 
-module.exports = function(element, styleProps) {
-  var style = element.style
+var style = module.exports = function(element, styleProps) {
+  var elStyle = element.style
   each(styleProps, function(val, key) {
     if (typeof val == 'number') { val = val + 'px' }
+    
     if (key == 'float') { key = 'cssFloat' }
-    if (module.exports.prefixed[key]) { key = module.exports.prefix + key }
-    if (val != null) { style[key] = val } // catches null and undefined
+    else if (key == 'gradient') { return style.gradient.apply(style, [element].concat(val.split(' '))) }
+    else if (key == 'opacity') { return style.opacity(element, val) }
+    else if (style.prefixed[key]) { key = style.prefix + key }
+    
+    if (val != null) { elStyle[key] = val } // catches null and undefined
   })
   return element
 }
 
-module.exports.prefix = 
+style.prefix = 
   client.isFirefox ? (client.version < 5 ? '-moz-' : '') :
   client.isWebKit ? '-webkit-' :
   ''
 
-module.exports.prefixed = {
+style.prefixed = {
   'boxShadow':true,
   'borderRadius':true,
   'transition':true
 }
 
-module.exports.opacity = function(el, fraction) {
+style.opacity = function(el, fraction) {
   if (client.isIE && client.version <= 8) {
-    module.exports.opacity = function(el, fraction) {
+    style.opacity = function(el, fraction) {
       el.style.filter = 'alpha(opacity=' + Math.round(fraction * 100) + ')'
     }
   } else {
-    module.exports.opacity = function(el, fraction) {
+    style.opacity = function(el, fraction) {
       el.style.opacity = fraction
       el.style.MozOpacity = fraction
       el.style.KhtmlOpacity = fraction
     }
   }
-  module.exports.opacity(el, fraction)
+  style.opacity(el, fraction)
 }
 
-module.exports.gradient = function(el, fromColor, toColor) {
+style.gradient = function(el, fromColor, toColor) {
   var key = 'background',
     val
   if (client.isIE && client.version <= 8) {
