15e31f261fea14ccdc7ff3724fb395aa89796918
Marcus Westin <marcus.westin@gmail.com>
2011-08-24 15:59:50 -0700

    Stub out keypad

---
+++ ios/KeyPad.js
@@ -0,0 +1,11 @@
+var Class = require('std/Class'),
+	Component = require('ui/dom/Component'),
+	Button = require('ui/ios/Button')
+
+module.exports = Class(Component, function() {
+	
+	this.renderContent = function() {
+		this.append()
+	}
+	
+})
