7fb44113c7c01bca51fead2dbfefaa94e35e8541
Marcus Westin <marcus.westin@gmail.com>
2011-08-24 15:59:03 -0700

    Make the button plain DOM, rather than a button

--- ios/Button.js
+++ ios/Button.js
@@ -14,17 +14,16 @@ module.exports = Class(Component, function() {
 	}
 
 	this.renderContent = function() {
-		this.append(
-			this._button = BUTTON({
-				click:this._clickHandler,
-				touchstart:bind(this, this._onTouchStart),
-				touchend:bind(this, this._onTouchEnd),
-				touchcancel:bind(this, this._onTouchCancel),
-				touchmove:bind(this, this._onTouchMove)
-			}, this._label)
-			// DIV({ style:{position:'absolute', top:0, left:'3.5%', width:'94%', 'borderRadius':8 } })
-			// http://admindaily.com/glossy-buttons-without-images-using-only-css3.html
-		)
+		this
+			.on('touchstart', bind(this, this._onTouchStart))
+			.on('touchend', bind(this, this._onTouchEnd))
+			.on('touchcancel', bind(this, this._onTouchCancel))
+			.on('touchmove', bind(this, this._onTouchMove))
+		
+		this.append(SPAN('label', this._label))
+
+		// DIV({ style:{position:'absolute', top:0, left:'3.5%', width:'94%', 'borderRadius':8 } })
+		// http://admindaily.com/glossy-buttons-without-images-using-only-css3.html
 	}
 	
 	this._onTouchStart = function(e) {
@@ -33,14 +32,14 @@ module.exports = Class(Component, function() {
 		e.cancel()
 		var offset = this.getOffset()
 		this._touchRect = new Rect(offset.left, offset.top, offset.width, offset.height).pad(10)
-		this._button.addClass('active')
+		this.addClass('active')
 	}
 	
 	this._onTouchMove = function(e) {
 		if (!this._touchRect) { return }
 		var touch = e.touches[0]
-		if (this._touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) { this._button.addClass('active') }
-		else { this._button.removeClass('active') }
+		if (this._touchRect.containsPoint({ x:touch.pageX, y:touch.pageY })) { this.addClass('active') }
+		else { this.removeClass('active') }
 	}
 	
 	this._onTouchEnd = function(e) { this._endTouch(e) }
@@ -49,8 +48,8 @@ module.exports = Class(Component, function() {
 		if (!this._touchRect) { return }
 		e.cancel()
 		delete this._touchRect
-		var shouldClick = this._button.hasClass('active')
-		this._button.removeClass('active')
+		var shouldClick = this.hasClass('active')
+		this.removeClass('active')
 		if (shouldClick) { this._clickHandler() }
 		// TODO deregister touch events
 	}
