c290b20d3a6c33ed8d3a1633feed9350d040430a
Marcus Westin <marcus.westin@gmail.com>
2011-08-24 19:45:59 -0700

    Implement the basics of a keypad which shows the dollar value of its input

--- ios/KeyPad.js
+++ ios/KeyPad.js
@@ -1,11 +1,51 @@
 var Class = require('std/Class'),
 	Component = require('ui/dom/Component'),
-	Button = require('ui/ios/Button')
+	round = require('std/math/round'),
+	slice = require('std/slice')
 
 module.exports = Class(Component, function() {
+
+	this._class = 'KeyPad'
+	
+	this.init = function() {
+		this._digits = []
+		this._keys = [
+			1, 2, 3,
+			4, 5, 6,
+			7, 8, 9,
+			'.',0,'<']
+	}
 	
 	this.renderContent = function() {
-		this.append()
+		this.append(
+			this._valueEl = DIV('value'),
+			this._keysEl = DIV('keys')
+		)
+		
+		each(this._keys, this, function(key) {
+			this._keysEl.append(new Button('key', key, bind(this, this._onKeyPress, key)))
+		})
+		
+		this._renderValue()
+	}
+	
+	this._onKeyPress = function(key) {
+		if (key == 0 && !this._digits.length) { /* do nothing */ }
+		else if (typeof key == 'number') { this._digits.push(key) }
+		else if (key == '<') { this._digits.pop() }
+		this._renderValue()
+	}
+	
+	this._renderValue = function() {
+		var digitsCopy = slice(this._digits)
+		if (digitsCopy.length == 0) { digitsCopy = [0,0,0] }
+		if (digitsCopy.length == 1) { digitsCopy = [0,0,digitsCopy[0]] }
+		if (digitsCopy.length == 2) { digitsCopy = [0,digitsCopy[0],digitsCopy[1]] }
+		digitsCopy.splice(digitsCopy.length - 2, 0, '.')
+
+		if (digitsCopy.length < 4) { digitsCopy.unshift(0) }
+		if (digitsCopy.length < 4) { digitsCopy.unshift(0) }
+		this._valueEl.getElement().innerHTML = '$' + digitsCopy.join('')
 	}
 	
 })
