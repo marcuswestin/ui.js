7f1b7facf13637083a30f5b613ddc5e6f748efb3
Marcus Westin <marcus.westin@gmail.com>
2011-08-26 16:33:00 -0700

    Make KeyPad extensible by subclassing

--- ios/KeyPad.js
+++ ios/KeyPad.js
@@ -8,22 +8,17 @@ module.exports = Class(Component, function() {
 	this._class = 'KeyPad'
 	
 	this.init = function() {
-		this._digits = []
-		this._keys = [
-			1, 2, 3,
-			4, 5, 6,
-			7, 8, 9,
-			'00',0,'<']
+		this._input = []
 	}
 	
 	this.clear = function() {
-		this._digits = []
+		this._input = []
 		if (this._el) { this._renderValue() }
 		return this
 	}
 	
-	this.getValue = function() {
-		return parseFloat(this._getValueString(), 10)
+	this.getInput = function() {
+		return slice(this._input)
 	}
 	
 	this.renderContent = function() {
@@ -38,6 +33,7 @@ module.exports = Class(Component, function() {
 			if (i < 3) { className += ' top' }
 			if ((i - 1) % 3 == 0) { className += ' center' }
 			if ((i + 1) % 3 == 0) { className += ' right' }
+			if (this._keys[i] === '') { className += ' empty' }
 			this._keysEl.append(new Button(className, key, bind(this, this._onKeyPress, key)))
 		})
 		
@@ -45,27 +41,20 @@ module.exports = Class(Component, function() {
 	}
 	
 	this._onKeyPress = function(key) {
-		if (key == 0 && !this._digits.length) { /* do nothing */ }
-		else if (typeof key == 'number') { this._digits.push(key) }
-		else if (key == '00') { this._digits.push(0); this._digits.push(0) }
-		else if (key == '<') { this._digits.pop() }
+		if (this._shouldAcceptInput(key)) { this._input.push(key) }
 		this._renderValue()
 	}
-	
-	this._renderValue = function() {
-		this._valueEl.getElement().innerHTML = '$' + this._getValueString()
-	}
-	
-	this._getValueString = function() {
-		var digitsCopy = slice(this._digits)
-		if (digitsCopy.length == 0) { digitsCopy = [0,0,0] }
-		if (digitsCopy.length == 1) { digitsCopy = [0,0,digitsCopy[0]] }
-		if (digitsCopy.length == 2) { digitsCopy = [0,digitsCopy[0],digitsCopy[1]] }
-		digitsCopy.splice(digitsCopy.length - 2, 0, '.')
 
-		if (digitsCopy.length < 4) { digitsCopy.unshift(0) }
-		if (digitsCopy.length < 4) { digitsCopy.unshift(0) }
-		return digitsCopy.join('')
-	}
+	this._renderValue = function() { this._valueEl.getElement().innerHTML = this._getFormattedValue() }
 	
+	/* Overwrite these for custom behavior
+	 *************************************/
+	this._shouldAcceptInput = function(digit) { return true }
+	this._getFormattedValue = function() { return this._input.join('') }
+
+	this._keys = [
+		1, 2, 3,
+		4, 5, 6,
+		7, 8, 9,
+		'',0,'']
 })
